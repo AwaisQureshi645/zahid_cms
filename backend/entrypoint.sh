453bc74355ad16a7c9aee51ad1afb602211673b4
AwaisQureshi645 <awaisqureshi645@gmail.com>
2025-11-08 16:49:47 +0500

    remoev file

--- backend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Get port from environment or use default
-PORT=${PORT:-8080}
-
-# Run gunicorn
-exec gunicorn wsgi:app --bind 0.0.0.0:$PORT --workers 2 --threads 2 --timeout 120
-
